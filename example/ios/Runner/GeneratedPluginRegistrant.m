2bf0cac917739cab7f13f5fdabb00ea2b15932eb
Dominik Roszkowski <kontakt@dominikroszkowski.pl>
2019-05-29 20:42:16 +0200

    Replace ObjC SDK with Swift version

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <flutter_bugfender/FlutterBugfenderPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterBugfenderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterBugfenderPlugin"]];
+}
+
+@end
